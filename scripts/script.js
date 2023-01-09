2fc69fa3d762ee85f87a5a253e26d81b8a887e20
JoshuaCDean <u684188640@byui.edu>
2023-01-08 19:08:11 -0600

    fixed time and stuff

--- scripts/script.js
+++ scripts/script.js
@@ -23,8 +23,13 @@ let months = [
 	"December"
 ];
 
-let d = new Date();
+let d = new Date(document.lastModified);
 let year = d.getFullYear();
-let fulldate = `Last Updated: ${d.getMonth()+1}/${d.getDate()}/${year} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
+let fulldate = `Last Updated: ${d.getMonth()+1}/${d.getDate()}/${year} ${minTwoDigits(d.getHours())}:${minTwoDigits(d.getMinutes())}:${minTwoDigits(d.getSeconds())}`
 
 document.getElementById("currentdate").textContent = fulldate;
+
+
+function minTwoDigits(n) {
+    return (n < 10 ? '0' : '') + n;
+}
