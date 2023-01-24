32a5f44323387fefa54c9b9073ece6e163999cd5
JoshuaCDean <u684188640@byui.edu>
2023-01-23 22:59:37 -0600

    Finished CCOC Site Plan

---
+++ lesson3/scripts/script.js
@@ -0,0 +1,35 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+
+let d = new Date(document.lastModified);
+let year = d.getFullYear();
+let fulldate = `Last Updated: ${d.getMonth()+1}/${d.getDate()}/${year} ${minTwoDigits(d.getHours())}:${minTwoDigits(d.getMinutes())}:${minTwoDigits(d.getSeconds())}`
+document.getElementById("copyyear").textContent= year;
+document.getElementById("currentdate").textContent = fulldate;
+
+
+function minTwoDigits(n) {
+    return (n < 10 ? '0' : '') + n;
+}
