a2c5c02f958f151d6bb1a9096a32b20c09f60984
JoshuaCDean <u684188640@byui.edu>
2023-02-04 19:02:02 -0600

    Fixes

--- domManipulation/scripts/scripts.js
+++ domManipulation/scripts/scripts.js
@@ -5,17 +5,27 @@ button.addEventListener("click", addChapter);
 function addChapter()
 {
     const favchap = document.querySelector("#favchap");
-    const list = document.querySelector("#list")
-    const item = favchap.value;
-    favchap.value = "";
-    const listElements = document.createElement("li")
-    listElements.innerHTML=item
-    list.appendChild(listElements);
-    
-    const delButton = document.createElement("button")
-    delButton.innerHTML = "X"
-    listElements.appendChild(delButton)
-    delButton.addEventListener("click", () => {
-        list.removeChild(listElements)
-    })
+    if (!isEmpty(favchap.value))
+    {
+        console.log(favchap.value)
+        const list = document.querySelector("#list")
+        const item = favchap.value;
+        favchap.value = "";
+        const listElements = document.createElement("li")
+        listElements.innerHTML=item
+        list.appendChild(listElements);
+
+        const delButton = document.createElement("button")
+        delButton.innerHTML = "X"
+        listElements.appendChild(delButton)
+        delButton.addEventListener("click", () => {
+            list.removeChild(listElements)
+        })
+    } else{
+        console.log("No Entry")
+    }
+}
+
+function isEmpty(str) {
+    return !str.trim().length;
 }
